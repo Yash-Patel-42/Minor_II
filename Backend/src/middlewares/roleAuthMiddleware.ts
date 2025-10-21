d6988b0f81774cac24ac5645e695337e623c7050
Yash Patel <yash422005patel@gmail.com>
2025-10-21 22:59:35 +0530

    Add: rolePermission schema setup done.

--- Backend/src/middlewares/roleAuthMiddleware.ts
+++ Backend/src/middlewares/roleAuthMiddleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
-export const roleAuth = async (requiredPermission: Permission) => {
+export const roleAuth = async (requiredPermission) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     //Get data from the req and middleware.
     const userRole = req.memberInfo?.role
